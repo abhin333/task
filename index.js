2cf96fd4a1b096152b7faa193616d3cb83647caf
abhin333 <abhinpradeepan123@gmail.com>
2024-04-29 02:54:58 -0700

    about-page

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('nav').innerHTML = data;
-        });
-}); 
